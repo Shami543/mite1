895b9460bfdb1d3b9fc421573f5aa7625c94bb82
Shami543 <96344848+Shami543@users.noreply.github.com>
2023-01-08 17:55:21 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class Palindrome {
+	public static void main(String[] args) {
+		String str = new java.util.Scanner(System.in).next();
+		char c[] = str.toUpperCase().toCharArray();
+		int len = str.length();
+		for(int i = 0; i < len / 2; i++) {
+			if(c[i] != c[len - i - 1]) {
+				System.out.println("NO");
+				return;
+			}	
+		}
+		System.out.println("YES");
+	}
+}

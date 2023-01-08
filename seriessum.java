7027f14a03ed5b1527d6718c6b984a0a12d0b2d7
Shami543 <96344848+Shami543@users.noreply.github.com>
2023-01-08 18:00:07 +0530

    Create seriessum.java

---
+++ seriessum.java
@@ -0,0 +1,19 @@
+package myPackage;
+
+public class SeriesSum {
+
+	public static void main(String[] args) {
+		int num = new java.util.Scanner(System.in).nextInt();
+		double fact = 1, result = 0;
+		for(int i = 1; i <= num; i ++) {
+			fact *= i;
+			if(i % 2 == 0)
+				result -= (i / fact);
+			else
+				result += (i / fact);
+			System.out.println(fact + " " + result);
+		}
+		System.out.println(result);
+	}
+
+}

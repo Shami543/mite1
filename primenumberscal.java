2d8e4dde99033c378a62aaa710eeb6784b77eaea
Shami543 <96344848+Shami543@users.noreply.github.com>
2023-01-08 17:57:52 +0530

    Create primenumberscal.java

---
+++ primenumberscal.java
@@ -0,0 +1,19 @@
+package myPackage;
+
+public class PrimeNumbersCal {
+	public static void main(String[] args) {
+		int n = new java.util.Scanner(System.in).nextInt();
+		int cnt = 0;
+		for(int i = 2; i <= n; i++) {
+			if(isPrime(i))
+				cnt++;
+		}
+		System.out.println(cnt);
+	}
+	public static boolean isPrime(int n) {
+		for(int i = 2; i * i <= n; i++)
+			if(n % i == 0)
+				return false;
+		return true;
+	}
+}
